3c33b24769bed093ff0b1a0b4c3f33f4a3f52387
SahirSood <sahirsood@gmail.com>
2023-06-01 21:50:18 -0700

    UPDATED  A2 ASSIGNMENT

--- assignments/a1/Wordlist.h
+++ assignments/a1/Wordlist.h
@@ -57,55 +57,27 @@ class Wordlist : public Wordlist_base
 
     public:
 
-        Wordlist(){}
+        Wordlist(){} // default list constructor
 
         Wordlist (string file_name){
             
             std::string word_;
-            ifstream file_(file_name);
+            ifstream file_(file_name); //opens the file given in .cpp file  or terminal
             if(file_.is_open())
             {
-                while(file_ >> word_){
-                    add_word(word_);    
-                    // Node* curNode = head;
-                    // while(curNode != nullptr){
-                    //     if(curNode->word == word_){
-                    //         curNode->count++;
-                    //         break;
-                    //         }
-                    //     curNode = curNode->next;
-                    //     }
-                    // if(curNode==nullptr){
-                    //     newnode = new Node;
-                    //     newnode->word = word_;
-                    //     newnode->count = 1;
-                    //     newnode->next = nullptr;
-
-                    //     if(head == nullptr || word_<head->word){
-                    //         newnode->next =head;
-                    //         head = newnode;
-                    //     }     
-                    //     else {
-                    //         curNode = head;
-                    //         while (curNode->next != nullptr && curNode->next->word<word_) {
-                    //             curNode = curNode->next;
-                    //         }
-                    //         newnode->next = curNode->next;
-                    //         curNode->next = newnode;
-                    
-                    //      }
-                    // }
+                while(file_ >> word_){ // loops thorugh each word in file and saves to local variable
+                    add_word(word_);   // calls function that adds word to LL
                  }
                 file_.close();
             }
             else{
-                cerr << "Error opening file: " << file_name << endl;\
+                cerr << "Error opening file: " << file_name << endl;
             }
         }
          int get_count(const string &w)const{
             Node* curNode = head;
             while (curNode != nullptr){
-                if (curNode->word == w){
+                if (curNode->word == w){ //If current node is the same as the parameter we return the count
                     return curNode->count;
                 }
                 else{
@@ -119,7 +91,8 @@ class Wordlist : public Wordlist_base
         int num_different_words()const{
             int total = 0;
             Node*curNode = head;
-            while(curNode!=nullptr){
+           
+            while(curNode!=nullptr){ //LL copmrised of only unique words, we count number of nodes
                 total++;
                 curNode = curNode->next;
             }
@@ -129,8 +102,9 @@ class Wordlist : public Wordlist_base
         int total_words()const{
             int total = 0;
             Node*curNode = head;
+            
             while(curNode!=nullptr){
-                total+=curNode->count;
+                total+=curNode->count; // add the count of each node, gives us summation of all total instances of each word
                 curNode = curNode->next;
             }
             return total;
@@ -138,13 +112,13 @@ class Wordlist : public Wordlist_base
 
         bool is_sorted()const{
             
-            if (head == nullptr) {
-                return true;
+            if (head == nullptr) { // return true if list is empty
+                return true; 
                 }
             
             Node*curNode = head; 
             while(curNode->next!=nullptr){
-                if (curNode->word > curNode->next->word)
+                if (curNode->word > curNode->next->word) // using ASCII format if the upcoming word if the next word is alphabetically less than current we return false
                 {
                     return false;
                 }
@@ -161,8 +135,8 @@ class Wordlist : public Wordlist_base
             Node*curNode = head;
             while(curNode!=nullptr){
                 if(curNode->count> max){
-                    ret = curNode->word;
-                    max = curNode->count;
+                    ret = curNode->word; // loops through and sets ret as the word with largest count
+                    max = curNode->count; // sets new count to be  tested against
                 }
             curNode = curNode->next;
             }
@@ -171,7 +145,8 @@ class Wordlist : public Wordlist_base
         int num_singletons()const{
             int total = 0;
             Node*curNode = head;
-            while(curNode!=nullptr){
+
+            while(curNode!=nullptr){ //every time we have a node with count 1 add it to total
                 if(curNode->count == 1){
                     total++;
                 }
@@ -181,73 +156,57 @@ class Wordlist : public Wordlist_base
         }
         void add_word(const string &w){
             Node* curNode = head;
-            while(curNode != nullptr){
-                if(curNode->word == w){
-                    curNode->count++;
-                    
-                    break;
+            while(curNode != nullptr){// loop through linked list
+                if(curNode->word == w){ 
+                    curNode->count++; //if word in the the list then add to count
+                    break; //leave loop
                     }
                 curNode = curNode->next;
                 }
-            if(curNode==nullptr){
+            if(curNode==nullptr){ // create new node given the word
                 Node* newnode = new Node;
                 newnode->word = w;
                 newnode->count = 1;
                 newnode->next = nullptr;
 
-                if(head == nullptr || w<head->word){
+                if(head == nullptr || w<head->word){ //if list is empty or new node should go before head
                     newnode->next = head;
                     head = newnode;
                 }     
                 else {
                     curNode = head;
-                    while (curNode->next != nullptr && curNode->next->word<w) {
+                    while (curNode->next != nullptr && curNode->next->word<w) { // goes to a point in the list wehre the next node is greater than w but curnode is less than
                         curNode = curNode->next;
                         ;
 
                     }
-                    newnode->next = curNode->next;
+                    newnode->next = curNode->next; // palces new node in between the 2 nodes
                     curNode->next = newnode;
             
                     }
             }
         }
-        void print_words()const{
+        void print_words()const{ //loops through entire list printing the word and ocunt
             Node*curNode = head;
+            
             while(curNode!=nullptr){
                 cout << "{" << "\""<< curNode->word<<"\"" <<curNode->count << "}"<<endl;
                 curNode = curNode->next;
             }
         } 
 
-    ~Wordlist(){
+    ~Wordlist(){ //clears and frees entire list
         Node* curNode = head;
         Node* del = head;
         
         while (curNode->next != nullptr)
         {
-            del = curNode;
-            curNode = curNode->next;
-            delete del;
+            del = curNode; // set node to delete with temp pointer
+            curNode = curNode->next;// iterate 
+            delete del; // delete the temp pointer
         }
         delete curNode;
     }
 
-    //
-    // ... you can write helper methods if you need them ...
-    //Testing git hub....
 
-    //
-    // ... write a default constructor that creates an empty Wordlist ...
-    //
-
-}; // class Wordlist
-
-//
-// ... you can write helper functions here (or before Wordlist) if you need them
-// ...
-//
-
-//
-// ... write functions to test every method of Wordlist ...
-//
+}; 
